e3c60d88d1d135404adf3c83f1f83e6224ed4d25
fadhilmuhaimin <fadilmuhaimin88@gmail.com>
2023-08-20 20:13:16 +0800

    add notifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_youtube/core/notifiers.dart';
 import 'package:flutter_youtube/pages/home_page.dart';
 import 'package:flutter_youtube/widget/widget_tree.dart';
 
@@ -12,13 +13,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          useMaterial3: true
-          ),
-        home: const WidgetTree());
+    return ValueListenableBuilder(
+        valueListenable: isDarkModeNotifier,
+        builder: (context, isDark, child) {
+          return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                brightness: isDark?  Brightness.dark : Brightness.light  ,
+                primarySwatch: Colors.blue, 
+                useMaterial3: true
+                ),
+              home: const WidgetTree());
+        });
   }
 }
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_youtube/core/constant.dart';
+import 'package:flutter_youtube/core/notifiers.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+      ),
+      body: const Column(
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage('images/yeah.png'),
+            radius: 60,
+          ),
+          SizedBox(
+            height: kDouble20,
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text("Flutter Map"),
+          ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text("info@fluttermapp.com"),
+          ),
+          ListTile(
+            leading: Icon(Icons.web),
+            title: Text("Flutter Mapp"),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        isDarkModeNotifier.value = !isDarkModeNotifier.value;
+      },
+      child: ValueListenableBuilder(valueListenable: isDarkModeNotifier, builder: (context, isDark, child) {
+        if(!isDark){
+          return const Icon(Icons.dark_mode);
+        }else{
+           return const Icon(Icons.light_mode);
+        }
+      },) ,
+      ),
+    );
   }
 }
